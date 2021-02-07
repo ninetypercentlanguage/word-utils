6b087c12349ba13e6ff98b3f3b27212b3d5c2524
Al Avery <al.avery.dev@gmail.com>
2021-02-07 16:11:50 -0600

    make lemma.Item public

--- lemma/lemma.go
+++ lemma/lemma.go
@@ -1,10 +1,11 @@
 package lemma
 
-type item struct {
+// Item is a single part of speech in a lemmas file
+type Item struct {
 	PartOfSpeech string   `json:"part_of_speech"`
 	Lemmas       []string `json:"lemmas"`
 	Exists       bool     `json:"exists"`
 }
 
 // Content of a lemmas file
-type Content []item
+type Content []Item
