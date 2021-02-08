0b9d25bcc23207192c9d7dba6dc3cf8088b443ca
Al Avery <al.avery.dev@gmail.com>
2021-02-07 19:43:56 -0600

    make combined.LemmaItem and ContentItem public

--- combined/combined.go
+++ combined/combined.go
@@ -1,14 +1,16 @@
 package combined
 
-type lemmaItem struct {
+// LemmaItem is single item in ContentItem Lemmas
+type LemmaItem struct {
 	Word        string   `json:"word"`
 	Definitions []string `json:"definitions"`
 }
 
-type contentItem struct {
+// ContentItem is single item in the array contained in a Content file
+type ContentItem struct {
 	PartOfSpeech string      `json:"part_of_speech"`
-	Lemmas       []lemmaItem `json:"lemmas"`
+	Lemmas       []LemmaItem `json:"lemmas"`
 }
 
 // Content is the content of a combined file
-type Content []contentItem
+type Content []ContentItem
